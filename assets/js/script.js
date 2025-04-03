cdd2b8b4d274151c7b4a1bec04a9ce2ddb9bc84e
UnnatiSurani14 <unnatis.encircle@gmail.com>
2025-04-03 19:11:31 +0530

    blue kingdom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,31 @@
-const swiper = new Swiper(".swiper", {
+const compnyContact = new Swiper(".compny-contact-swiper", {
   slidesPerView: 1,
   loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
   breakpoints: {
-    320: { slidesPerView: 1.25 },
+    320: { slidesPerView: 1.3 },
     425: { slidesPerView: 2 },
     660: { slidesPerView: 2.5 },
     1024: { slidesPerView: 4 },
     1280: { slidesPerView: 5 },
+  },
+});
+const populerExcursions = new Swiper(".populer-excursions-swiper", {
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 40,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: false,
+    clickable: true,
+  },
+  breakpoints: {
+    320: { slidesPerView: 1.25 },
+    660: { slidesPerView: 2 },
+    1024: { slidesPerView: 2.5 },
+    1280: { slidesPerView: 4 },
   },
 });
