bee8f96360f110145a199942d23512460d493d97
UnnatiSurani14 <unnatis.encircle@gmail.com>
2025-04-04 18:31:09 +0530

    blur kingdom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 // toggle
-let toggle=document.getElementById("toggle");
-let nav=document.getElementById("navbar");
+let toggle = document.getElementById("toggle");
+let nav = document.getElementById("navbar");
 
-toggle.addEventListener("click",function(){
+toggle.addEventListener("click", function () {
   if (nav.style.display === "none" || nav.style.display === "") {
     nav.style.display = "block";
-    nav.style.backgroundColor="#142C58";
+    nav.style.backgroundColor = "#142C58";
   } else {
     nav.style.display = "none";
   }
-})
+});
 
 // compny-contact
 const compnyContact = new Swiper(".compny-contact-swiper", {
@@ -45,3 +45,25 @@ const populerExcursions = new Swiper(".populer-excursions-swiper", {
     1280: { slidesPerView: 4 },
   },
 });
+
+// tab-slider
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll(".tab-button");
+  const contents = document.querySelectorAll(".tab-content");
+
+  buttons[0].style.borderBottom = "2px solid #142C58";
+  contents[0].classList.remove("hidden");
+
+  buttons.forEach((button, i) => {
+
+    button.addEventListener("click", function () {
+      contents.forEach((content) => content.classList.add("hidden"));
+      const target = document.getElementById(this.dataset.tab);
+      target.classList.remove("hidden");
+
+      buttons.forEach((btn) => (btn.style.borderBottom = "none"));
+      this.style.borderBottom = "2px solid #142C58";
+    });
+  });
+});
+
